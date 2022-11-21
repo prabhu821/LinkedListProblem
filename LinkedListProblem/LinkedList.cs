1c547a79c79582522d6cc7c25581693851ed9278
rishiraj007 <rishiraj@mounttalent.com>
2022-11-21 18:34:03 +0530

    [Prabhu] Add: Add Data

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -8,6 +8,40 @@ namespace LinkedListProblem
 {
     internal class LinkedList
     {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next!=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while(temp!=null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
         internal void DeleteNodeAtParticularPosition(int position)
         {
             if(this.head==null)
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -6,22 +6,26 @@ namespace LinkedListProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome tp Linked List operation");
-        //    LinkedListStack stack =new LinkedListStack();
-        //    stack.Push(50);
-        //    stack.Push(70);
-        //    stack.Push(30);
-        //    stack.Display();
-        //    stack.Peek();
-        //    stack.Pop();
-        //    stack.Display();
+            LinkedList list = new LinkedList();
+            list.Add(50);
+            list.Add(70);
+            list.Add(30);
+            //    LinkedListStack stack =new LinkedListStack();
+            //    stack.Push(50);
+            //    stack.Push(70);
+            //    stack.Push(30);
+            //    stack.Display();
+            //    stack.Peek();
+            //    stack.Pop();
+            //    stack.Display();
 
-            LinkedListQueue queue = new LinkedListQueue();
-            queue.Enqueue(50);
-            queue.Enqueue(70);
-            queue.Enqueue(30);
-            queue.Display();
-            queue.Dequeue();
-            queue.Display();
+            //    LinkedListQueue queue = new LinkedListQueue();
+            //    queue.Enqueue(50);
+            //    queue.Enqueue(70);
+            //    queue.Enqueue(30);
+            //    queue.Display();
+            //   queue.Dequeue();
+            //    queue.Display();
         }
 
     }
