a0f74b5ffca1e01016520ccaebc4f61a291bf879
rishiraj007 <rishiraj@mounttalent.com>
2022-11-21 18:17:38 +0530

    [Prabhu] Add: Linked List

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class LinkedList
+    {
+        internal void DeleteNodeAtParticularPosition(int position)
+        {
+            if(this.head==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            Node temp = this.head;
+            if(position==0)
+            {
+                this.head = temp.next;
+                return;
+            }
+            for(int i=0;temp!=null && i<position-1;i++)
+            {
+                temp=temp.next;
+            }
+            Node next = temp.next.next;
+            temp.next = next;
+            Size();
+        }
+        internal void Size()
+        {
+            Node temp = this.head;
+            int count = 0;
+            if(temp==null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            if(temp!=null)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome tp Linked List operation");
+        //    LinkedListStack stack =new LinkedListStack();
+        //    stack.Push(50);
+        //    stack.Push(70);
+        //    stack.Push(30);
+        //    stack.Display();
+        //    stack.Peek();
+        //    stack.Pop();
+        //    stack.Display();
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(50);
+            queue.Enqueue(70);
+            queue.Enqueue(30);
+            queue.Display();
+            queue.Dequeue();
+            queue.Display();
+        }
+
+    }
+}
