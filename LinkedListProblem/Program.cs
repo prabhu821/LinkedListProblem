77e3356d11191456fb6b7f7d2b60d999e2f980d3
rishiraj007 <rishiraj@mounttalent.com>
2022-11-22 18:21:18 +0530

    [Prabhu] Add: Stack created

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -5,41 +5,42 @@ namespace LinkedListProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome tp Linked List operation");
+            Console.WriteLine("Welcome to Linked List operation");
             
-            LinkedList list = new LinkedList();
-            list.Add(50);
-            list.Add(70);
-            list.Add(30);
-            list.Display();
-            int a = list.Search(30);
-            list.InsertAtParticularPosition(a + 1, 40);
-            list.Display();
-            list.DeleteNodeAtParticularPosition(2);
-            list.Size();
-            list.Display();
+            //  LinkedList list = new LinkedList();
+            //  list.Add(50);
+            //  list.Add(70);
+            //  list.Add(30);
+            //  list.Display();
+            //  int a = list.Search(30);
+            //  list.InsertAtParticularPosition(a + 1, 40);
+            //  list.Display();
+            //  list.DeleteNodeAtParticularPosition(2);
+            //  list.Size();
+            //  list.Display();
 
 
 
-            //    LinkedListStack stack =new LinkedListStack();
-            //    stack.Push(50);
-            //    stack.Push(70);
-            //    stack.Push(30);
-            //    stack.Display();
-            //    stack.Peek();
-            //    stack.Pop();
-            //    stack.Display();
+              LinkedListStack stack =new LinkedListStack();
+              stack.Push(50);
+              stack.Push(70);
+              stack.Push(30);
+              stack.Display();
+            //  stack.Peek();
+            //  stack.Pop();
+            //  stack.Display();
 
 
 
-            //    LinkedListQueue queue = new LinkedListQueue();
-            //    queue.Enqueue(50);
-            //    queue.Enqueue(70);
-            //    queue.Enqueue(30);
-            //    queue.Display();
-            //   queue.Dequeue();
-            //    queue.Display();
+            //  LinkedListQueue queue = new LinkedListQueue();
+            //  queue.Enqueue(50);
+            //  queue.Enqueue(70);
+            //  queue.Enqueue(30);
+            //  queue.Display();
+            //  queue.Dequeue();
+            //  queue.Display();
+        
+        
         }
-
     }
 }
