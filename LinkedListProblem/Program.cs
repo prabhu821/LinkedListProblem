05cc9c93bde4475d2529939691dd04eef4fa4f16
rishiraj007 <rishiraj@mounttalent.com>
2023-06-19 23:28:52 +0530

    [Prabhu]Add: Create linked list

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Linked List Problem!");
+            LinkedList list = new LinkedList();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.Create Linked List \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        list.Add(56);
+                        list.Add(30);
+                        list.Add(70);
+                        break;
+
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
