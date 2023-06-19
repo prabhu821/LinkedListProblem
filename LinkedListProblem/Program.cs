f4436b483c1e21c46f941ea6aa0d48d2d5e0d09c
rishiraj007 <rishiraj@mounttalent.com>
2023-06-19 23:23:30 +0530

    [Prabhu]Add: Linked List Problem

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Problem!");
+        }
+    }
+}
